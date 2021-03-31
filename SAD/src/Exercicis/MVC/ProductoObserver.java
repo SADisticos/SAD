1b47641996ef1432061b7448ddeb3ded7e2918b9
evilmonkey19 <eperpinyapitarch@gmail.com>
2021-03-31 22:36:28 +0200

    MVC Done

---
+++ SAD/src/Exercicis/MVC/ProductoObserver.java
@@ -0,0 +1,19 @@
+package Exercicis.MVC;
+
+import java.util.Observable;
+import java.util.Observer;
+import Exercicis.MVC.Producto.PrecioEvent;
+/**
+ *
+ * @author Enric
+ */
+public class ProductoObserver implements Observer{
+    
+    @Override
+    public void update(Observable observable, Object args){
+        if(args instanceof PrecioEvent){
+            PrecioEvent evento = (PrecioEvent) args;
+            System.out.printf("El producto %s ha cambiado de precio %s a %s%n", evento.getProducto().getNombre(), evento.getPrecioAntiguo(), evento.getPrecioNuevo());
+        }
+    }
+}
