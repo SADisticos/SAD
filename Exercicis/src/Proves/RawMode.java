5bd219d4d18360876e88bac530ef87d0bacea1bc
evilmonkey19 <eperpinyapitarch@gmail.com>
2021-04-01 20:33:45 +0200

    Raw Mode Test

--- Exercicis/src/Proves/RawMode.java
+++ Exercicis/src/Proves/RawMode.java
@@ -2,15 +2,22 @@ package Proves;
 
 import ConsoleBars.Const;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Enric
  */
 public class RawMode {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args){
         //System.out.print(Const.CSI + "3h");
-        Runtime.getRuntime().exec("echo hola");
-        
+        try{
+            Runtime.getRuntime().exec("echo hola");
+        } catch (IOException ex) {
+            Logger.getLogger(RawMode.class.getName()).log(Level.SEVERE, null, ex);
+        } finally{
+            System.out.print("Pollas");
+        }
     }
 }
