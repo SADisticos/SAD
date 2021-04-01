94c306621f01ee24cba2a8e31d1cff3d86379619
evilmonkey19 <eperpinyapitarch@gmail.com>
2021-04-01 20:31:23 +0200

    Add Constants file

---
+++ Exercicis/src/ConsoleBars/Const.java
@@ -0,0 +1,13 @@
+package ConsoleBars;
+
+/**
+ *
+ * @author Enric
+ */
+public class Const {
+    
+    public static final String CSI = "\033["; // ESC [
+    public static final String ERASELINE = "2K";
+    public static final String CURSORINITLINE = "G";
+    public static final char BEEP = '\007';
+}
