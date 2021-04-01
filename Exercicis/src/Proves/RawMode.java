d1c7b2ea1a01ac763ce7209dbf2a20628010c72a
evilmonkey19 <eperpinyapitarch@gmail.com>
2021-04-01 23:28:03 +0200

    Raw Mode Done

--- Exercicis/src/Proves/RawMode.java
+++ Exercicis/src/Proves/RawMode.java
@@ -1,6 +1,5 @@
 package Proves;
 
-import ConsoleBars.Const;
 import java.io.Console;
 import java.io.IOException;
 import java.io.Reader;
@@ -12,14 +11,14 @@ import java.io.Reader;
 public class RawMode {
     public static void main(String[] args) throws IOException, InterruptedException{
         String[] cmd = {"/bin/sh", "-c", "stty raw </dev/tty"};
-        // System.out.print(Const.CSI + "3h");
         Runtime.getRuntime().exec(cmd).waitFor();
         Console console = System.console();
         Reader reader = console.reader();
-        StringBuilder password = new StringBuilder();
+        StringBuilder line = new StringBuilder();
+        int character = 0;
         System.out.println("Enter your 8 character password: ");
-        for (int i = 0; i < 8; i++)
-            password.append(reader.read());
+        while((character = reader.read()) != -1)
+            line.append((char)character);
         cmd = new String[] {"/bin/sh", "-c", "stty sane </dev/tty"};
         Runtime.getRuntime().exec(cmd).waitFor();
     }
