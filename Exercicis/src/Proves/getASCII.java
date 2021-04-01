f130df9cfec0dd92529ccff153ec1e70a5113608
evilmonkey19 <eperpinyapitarch@gmail.com>
2021-04-02 00:51:33 +0200

    Bug solved

--- Exercicis/src/Proves/getASCII.java
+++ Exercicis/src/Proves/getASCII.java
@@ -10,15 +10,28 @@ public class getASCII{
 		String[] cmd = {"/bin/sh", "-c", "stty raw </dev/tty"};
 		Runtime.getRuntime().exec(cmd).waitFor();
 		Reader reader = System.console().reader();
-		int character1 = 0;
-		int character2 = 0; // saved character
-		while((character1 = reader.read()) != 0x0D) // Carriage Return
-			if(character1 != character2){
+		int ch = 0;  // character
+		int sch = 0; // saved character
+		StringBuilder line = new StringBuilder();
+		System.out.println("Linea 1");
+		System.out.print(Const.CSI + Const.CURSORINITLINE);
+		System.out.print("Linea 2");
+		System.out.print(Const.CSI + "F");
+		Thread.sleep(2000);
+		while((ch = reader.read()) != 0x0D){ // Carriage Return
+			line.append((char) ch); // For special character supress (char) and convert on ASCII table
+			System.out.print(Const.CSI + Const.ERASELINE);
+			System.out.print(line.toString());
+			System.out.print(Const.CSI + Const.CURSORINITLINE);
+			
+			if(ch != sch ){
+				System.out.print(Const.CSI + "E");
 				System.out.print(Const.CSI + Const.ERASELINE);
-				System.out.print(Const.CSI + Const.CURSORINITLINE);
-				System.out.print(character1);
-				character2 = character1;
+				System.out.print(ch);
+				System.out.print(Const.CSI + "F");
+				sch = ch;
 			}
+		}
 		cmd = new String[] {"/bin/sh", "-c", "stty sane </dev/tty"};
 		Runtime.getRuntime().exec(cmd).waitFor();
 	}
