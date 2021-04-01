a7c5929093f2b6d5787c7059be452e2462596a38
evilmonkey19 <eperpinyapitarch@gmail.com>
2021-04-01 18:50:40 +0200

    To Test

--- Exercicis/src/ConsoleBars/ConsolePercent.java
+++ Exercicis/src/ConsoleBars/ConsolePercent.java
@@ -23,31 +23,41 @@ class ConsolePercent implements Observer {
 		model = value;
 		
 		// get max columns
-		cmax = getColumns();
+		cmax = Proves.TerminalWidth.getColumns();
 		// write 0%
-		...
+                model.setMax(cmax);
+		System.out.println(model.get() + "%");      // Starts at 0
 	}
 	
 	int getMax() {
 		return cmax;
 	}
 	
+        @Override
 	public void update(Observable o, Object arg) {
 		restore();
 		Command comm = (Command) arg;
 		switch (comm.op) {
 			case INC:
+                            model.inc();
+                            break;
 			case DEC:
-				// write Value%
-				break;
+                            model.dec();
+                            break;
 			case BELL:
-				System.out.print('\007');
-				break;
+                            System.out.print(BEEP);
+                            break;
 		}
+                System.out.println(model.get() + "%");
 	}
-        private static void getColumns(){
-            Process p = 
-            
+        
+        private static final String CSI = "\033[";
+        private static final String ERASELINE = "2K";
+        private static final String CURSORINITLINE = "G";
+        private static final char BEEP = '\007';
+        
+        private void restore(){
+            System.out.print(CSI + ERASELINE);
+            System.out.print(CSI + CURSORINITLINE);
         }
-        private void restore(){}
 }
