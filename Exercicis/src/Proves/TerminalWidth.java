cfec4d1372abc3c5aed2de8991421d1482ddaa26
evilmonkey19 <eperpinyapitarch@gmail.com>
2021-04-01 17:21:39 +0200

    TerminalWidth DONE

--- Exercicis/src/Proves/TerminalWidth.java
+++ Exercicis/src/Proves/TerminalWidth.java
@@ -10,7 +10,7 @@ import java.io.InputStreamReader;
  */
 public class TerminalWidth {
     public static void main(String[] args){
-        
+        System.out.println(getColumns()); 
     }
     
     public static int getColumns(){
@@ -18,9 +18,12 @@ public class TerminalWidth {
             Process p = Runtime.getRuntime().exec("tput cols");
             p.waitFor();
             BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            
+            String line = reader.readLine();  // There is only one line to read
+            return Integer.parseInt(line);
         } catch(IOException | InterruptedException e){
             System.out.println(e.getMessage());
         }
-        
+        return 0; // To make the compiler shut up
     }
 }
