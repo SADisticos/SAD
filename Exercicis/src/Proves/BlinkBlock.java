66087751f30053a2afa06f27ba9f9f017c1cff7d
evilmonkey19 <eperpinyapitarch@gmail.com>
2021-04-03 13:42:16 +0200

    Eliminate 2 char backwards

--- Exercicis/src/Proves/BlinkBlock.java
+++ Exercicis/src/Proves/BlinkBlock.java
@@ -3,8 +3,13 @@ package Proves;
 import ConsoleBars.Const;
 
 public class BlinkBlock{
-    public static void main (String[] args){
+    public static void main (String[] args) throws InterruptedException{
         for(int i = 0; i < 3; i++)
             System.out.print(Const.BLOCK);
+        Thread.sleep(2000);
+        for(int i = 0; i < 2; i++){
+            System.out.print(Const.CSI + "D");
+            System.out.print(Const.CSI + "P");
+        }
     }
 }
