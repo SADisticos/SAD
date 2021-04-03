1ad926507635337d0100c22c3e83cf9e775e20cf
evilmonkey19 <eperpinyapitarch@gmail.com>
2021-04-03 13:35:53 +0200

    Block ASCII

---
+++ Exercicis/src/Proves/BlinkBlock.java
@@ -0,0 +1,10 @@
+package Proves;
+
+import ConsoleBars.Const;
+
+public class BlinkBlock{
+    public static void main (String[] args){
+        for(int i = 0; i < 3; i++)
+            System.out.print(Const.BLOCK);
+    }
+}
