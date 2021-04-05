f22d598fd0462cb9a4c509be288e191229a17f12
evilmonkey19 <eperpinyapitarch@gmail.com>
2021-04-05 14:55:01 +0200

    In progress

--- Practica_1/src/Basic/TestReadLine.java
+++ Practica_1/src/Basic/TestReadLine.java
@@ -1,5 +1,6 @@
 package Basic;
 
+import Other.Const;
 import java.io.*;
 /**
  *
@@ -10,8 +11,9 @@ public class TestReadLine {
     public static void main(String[] args){
         BufferedReader in = new EditableBufferedReader(new InputStreamReader(System.in));
         String str = null;
+        EditableBufferedReader.setRaw();
+        init();
         try{
-            EditableBufferedReader.setRaw();
             str = in.readLine();
         } catch(IOException ex){
             ex.printStackTrace();
@@ -19,5 +21,18 @@ public class TestReadLine {
             EditableBufferedReader.unsetRaw();
         }
         System.out.println("\nline is: " + str);
+    }
+    
+    private static void init(){
+        try{
+            System.out.print("There special characters. Be careful because some special char combination may be not available.");
+            Thread.sleep(2000);
+            System.out.print(Const.CURSORINITLINE);
+            System.out.print(Const.ERASELINE);
+            System.out.print(Const.CURSORINITLINE);
+        } catch(InterruptedException ex){
+            ex.printStackTrace();
+            System.exit(0);
+        }     
     }
 }
