d3f50d0c72d03e5024736b6c082572f57e6e2e3f
evilmonkey19 <eperpinyapitarch@gmail.com>
2021-04-07 00:57:14 +0200

    Trie Done

--- Practica_1/src/Other/Trie.java
+++ Practica_1/src/Other/Trie.java
@@ -14,29 +14,33 @@ import java.util.stream.Collectors;
 public class Trie {
 
     // Alphabet size (# of symbols)
-    static final int ASCII = 127;
+    public static final int ASCII = 127;
 
     
     // trie node
-    static class TrieNode{
+    public static class TrieNode{
         TrieNode[] children = new TrieNode[ASCII];
         
         // isEndOfWord is true if the node represents end of a word
         boolean isEndOfWord;
         
-        TrieNode(){
+        // Action to be done
+        int option;
+        
+        public TrieNode(){
             isEndOfWord = false;
+            option = Const.option.YES;
             for (int i = 0; i < ASCII; i++)
                 children[i] = null;
         }
     };
     
-    static TrieNode root;
+    public static TrieNode root;
     
     // If not present, inserts key into trie
     // If the key is prefix of trie node,
     // just marks leaf node
-    static void insert(String key){
+    public static void insert(String key, int option){
         int level;
         int length = key.length();
         int index;
@@ -55,10 +59,11 @@ public class Trie {
         
         // mark last node as leaf
         pCrawl.isEndOfWord = true;
+        pCrawl.option = option;
     }
     
     // Returns true if key presents in trie, else false
-    static boolean search(String key){
+    public static int search(String key){
         int level;
         int length = key.length();
         int index;
@@ -68,39 +73,60 @@ public class Trie {
             index = key.charAt(level);
             
             if(pCrawl.children[index] == null)
-                return false;
+                return Const.option.NO;
+                
             
             pCrawl = pCrawl.children[index];
         }
         
-        return (pCrawl != null && pCrawl.isEndOfWord);
+        return (pCrawl != null)? pCrawl.option: Const.option.NO;
     }
     
     public static void initTree(){
-        List<String> constantValues = Arrays.stream(Const.Trie.class.getDeclaredFields())
+        List<String> sequences = Arrays.stream(Const.sequence.class.getDeclaredFields())
                 .filter(field -> Modifier.isStatic(field.getModifiers()))
                 .map(field-> {
                     try{
-                        return (String) field.get(Const.Trie.class);
+                        return (String) field.get(Const.sequence.class);
                     } catch(IllegalAccessException ex){
                         throw new RuntimeException(ex);
                     }
                 })
                 .filter(name -> ! name.equals("NOT_NEEDED_CONSTANT")) // filter out if needed
                 .collect(Collectors.toList());
-        ListIterator it = constantValues.listIterator();
-        while(it.hasNext())
-            insert((String) it.next());
+        
+        List<Integer> options = Arrays.stream(Const.option.class.getDeclaredFields())
+                .filter(field -> Modifier.isStatic(field.getModifiers()))
+                .map(field-> {
+                    try{
+                        return (Integer) field.get(Const.option.class);
+                    } catch(IllegalAccessException ex){
+                        throw new RuntimeException(ex);
+                    }
+                })
+                .filter(name -> ! name.equals("NOT_NEEDED_CONSTANT")) // filter out if needed
+                .collect(Collectors.toList());
+        
+        ListIterator its = sequences.listIterator();
+        ListIterator ito = options.listIterator();
+        
+        while(its.hasNext() && ito.hasNext())
+            insert((String) its.next(), (int) ito.next());
     }
+    
     /*
     public static void main(String[] args){
         root = new TrieNode();
         initTree();
         
-        if(search(Const.CSI + Const.LEFT) == true) 
-            System.out.println("Yep! Funciona!!!");
-        else 
-            System.out.println("Me cago en todo!!!!!");
-    }
-    */   
+        StringBuilder sb = new StringBuilder();
+        
+        int option = Const.option.YES;
+        
+        while(option == Const.option.YES){
+            option = search(sb.append(String.valueOf(Const.ESC)).toString());
+            System.out.println(option);
+        }
+        System.out.println(option);
+    }*/  
 }
